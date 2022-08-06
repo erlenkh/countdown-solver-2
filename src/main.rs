3459adbb7055d4d86ff193a7b8ad0dcd0b517391
Erlend Kilvik Hoff <erlend.hoff@inventas.no>
2022-08-06 13:55:46 +0200

    made print_valid_words its own function, added short description of each function

--- src/main.rs
+++ src/main.rs
@@ -14,44 +14,48 @@ fn main() {
 
     let dict = load_dictionary("src/sortedDic.txt");
 
-    // Print out all n letter words that can be made from letter sequence
+    print_valid_words(&letter_seq, &dict);
+}
+
+/// Prints out all valid n letter words that can be made from letter sequence,
+/// where n goes from 1 to length of letter sequence.
+fn print_valid_words(letter_seq: &Vec<char>, dict: &HashSet<String>) {
     for n in 1..letter_seq.len()+1 {
         let permutations: Vec<String> = n_letter_perms(&letter_seq, n);
         
         let valid_words = dict_words(permutations, &dict);
 
-        println!("{n} letter words:");
-        print_words(&valid_words); 
-    }
-}
+        println!("Valid {n} letter words:");
 
-fn print_words(words: &Vec<String>) {
-    if words.len() > 0 {
-        for word in words {
-            println!("{word}");
+        if valid_words.len() > 0 {
+            for word in valid_words {
+                println!("{word}");
+            }
+        } else {
+            println!("(No words found)");
         }
-    } else {
-        println!("(No words found)")
+    
+        println!("\n");
     }
-
-    println!("\n");
 }
 
+/// Returns all n letter permutations of letter sequence.
 fn n_letter_perms(letter_seq: &Vec<char>, n: usize) -> Vec<String> {
     letter_seq.iter()
             .permutations(n)
             .unique()
-            .map(|x| x.into_iter().collect()) // converts Vec<char> to String
+            .map(|x| x.into_iter().collect()) // Converts Vec<char> to String
             .collect()
 }
 
+/// Filters out all non-dictionary words from Vector of letter sequence.
 fn dict_words(letter_seqs: Vec<String>, dict: &HashSet<String>) -> Vec<String> {
     letter_seqs.into_iter()
             .filter(|x| dict.contains(x))
             .collect()
 }
 
-
+/// Loads dictionary from filename into a HashSet of dictionary words.
 fn load_dictionary(filename: &str) -> HashSet<String> {
     let file = fs::File::open(filename)
                         .expect("No such file");
